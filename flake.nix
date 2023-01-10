aa111543b9aca1bbd3647d8b655b5a81af93b90d
Max <max@privatevoid.net>
2023-01-10 22:44:47 +0100

    follow nixpak's nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     nixpak.url = "github:nixpak/nixpak";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpak/nixpkgs";
     };
   };
   outputs = { flake-parts, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
