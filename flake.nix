15fd33d2eda77e7a33f3b667fbe27c34d7716942
Max <max@privatevoid.net>
2023-06-24 18:00:08 +0200

    meta: add hercules-ci-effects

--- flake.nix
+++ flake.nix
@@ -6,6 +6,13 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpak/nixpkgs";
     };
+    hercules-ci-effects = {
+      url = "github:hercules-ci/hercules-ci-effects";
+      inputs = {
+        nixpkgs.follows = "nixpak/nixpkgs";
+        flake-parts.follows = "flake-parts";
+      };
+    };
   };
   outputs = { flake-parts, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
     imports = [
