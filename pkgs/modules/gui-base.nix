2e02444661b502f67847365e7aac3924c02734b2
mae <26093674+MaeIsBad@users.noreply.github.com>
2024-10-20 20:30:20 +0200

    Replace envSuffix with concat and xdg sloth functions

--- pkgs/modules/gui-base.nix
+++ pkgs/modules/gui-base.nix
@@ -1,9 +1,4 @@
 { config, lib, pkgs, sloth, ... }:
-
-let
-  envSuffix = envKey: suffix: sloth.concat' (sloth.env envKey) suffix;
-in
-
 {
   config = {
     dbus.policies = {
@@ -23,7 +18,7 @@ in
       network = lib.mkDefault false;
       bind.rw = [
         [
-          (envSuffix "HOME" "/.var/app/${config.flatpak.appId}/cache")
+          sloth.appCacheDir
           sloth.xdgCacheHome
         ]
         (sloth.concat' sloth.xdgCacheHome "/fontconfig")
@@ -35,12 +30,12 @@ in
           (sloth.env "WAYLAND_DISPLAY")
         ])
 
-        (envSuffix "XDG_RUNTIME_DIR" "/at-spi/bus")
-        (envSuffix "XDG_RUNTIME_DIR" "/gvfsd")
-        (envSuffix "XDG_RUNTIME_DIR" "/pulse")
+        (sloth.concat' sloth.runtimeDir "/at-spi/bus")
+        (sloth.concat' sloth.runtimeDir "/gvfsd")
+        (sloth.concat' sloth.runtimeDir "/pulse")
       ];
       bind.ro = [
-        (envSuffix "XDG_RUNTIME_DIR" "/doc")
+        (sloth.concat' sloth.runtimeDir "/doc")
         (sloth.concat' sloth.xdgConfigHome "/gtk-2.0")
         (sloth.concat' sloth.xdgConfigHome "/gtk-3.0")
         (sloth.concat' sloth.xdgConfigHome "/gtk-4.0")
